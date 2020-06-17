020626863f67881580d772ce6c77c07801ad1948
monxx <prevost.kev@gmail.com>
2020-06-17 10:43:52 +0200

    add count test

--- javascript/app.js
+++ javascript/app.js
@@ -3,23 +3,32 @@
 
 const filterAnimals = (data, filterTerm) => {
 
+    const filterAnimalName = (animal) => {
+        return animal.name.match(new RegExp(filterTerm));
+    }
+
     return data.filter(country => {
         country.people = country.people.filter(
             person => {
-                person.animals=person.animals.filter(animal => animal.name.match(new RegExp(filterTerm)));
-
+                person.animals = person.animals.filter(animal => filterAnimalName(animal));
                 return person.animals.length > 0
             });
         return country.people.length > 0;
     });
 };
 
-const cmd = (input,arg) =>{
+const count = (data) => {
+
+};
+
+const cmd = (input, arg) => {
     let filterRegex = /^(--filter=(.+))$/;
     if (arg.match(filterRegex)) {
         return filterAnimals(input, arg.match(filterRegex)[2]);
     }
-    return [];
+
+
+    return input;
 }
 
-module.exports = {filterAnimals,cmd}
+module.exports = {filterAnimals, count, cmd}
