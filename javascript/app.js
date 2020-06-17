b7543abd7d3b4760e874e061507cdbed3f08174b
monxx <prevost.kev@gmail.com>
2020-06-17 10:20:48 +0200

    feature filter Animals

--- javascript/app.js
+++ javascript/app.js
@@ -6,6 +6,8 @@ const filterAnimals = (data, filterTerm) => {
     return data.filter(country => {
         country.people = country.people.filter(
             person => {
+                person.animals=person.animals.filter(animal => animal.name.match(new RegExp(filterTerm)));
+
                 return person.animals.length > 0
             });
         return country.people.length > 0;
